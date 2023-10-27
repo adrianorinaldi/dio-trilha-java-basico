087e205e981f70e0c8d0b4181e9f83a80842f6b8
adrianorinaldi <adriano-rinaldi@hotmail.com>
2023-10-26 21:13:02 -0300

    commit 3

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! teste");
+        Integer numero;
+        Integer agencia;
+        String nomeCliente;
+        Double saldo;
+        Scanner scanner =  new Scanner(System.in);
+
+        System.out.println("Digite o número da conta:");
+        numero = scanner.nextInt();
+        System.out.println("Digite a agência:");
+        agencia = scanner.nextInt();
+        System.out.println("Digite o seu nome:");
+        nomeCliente = scanner.next();
+        System.out.println("Digite o saldo:");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+ nomeCliente + ", " +
+                           "obrigado por criar uma conta em nosso banco, " +
+                           "sua agência é " + agencia + ", conta " + numero +" e seu saldo " + saldo + 
+                           " já está disponível para saque");
     }
 }
